2495ed3543bece5f8455b9b06828b9e71b43ebec
Rafi Khan <zetoslab@gmail.com>
2015-04-11 10:18:29 -0600

    Added js template

---
+++ plugin.js
@@ -0,0 +1,40 @@
+
+'use strict';
+
+module.exports = function(game, opts) {
+  return new FooPlugin(game, opts);
+};
+
+function FooPlugin(game, opts) {
+  // Functionality of plugin
+
+  // Dependencies
+  /* example:
+  this.mesherPlugin = game.plugins.get('voxel-mesher');
+  if(!this.mesherPlugin) throw new Error('voxel-foo requires voxel-mehser');
+  */
+  // Adds voxel-mesher as a requirement for voxel-foo
+
+  // Load options
+  /* eg
+  this.showOutline = opts.showOutline !== undefined ? opts.showOutline : true;
+  */
+
+  this.enable();
+}
+
+FooPlugin.prototype.enable = function() {
+  var self = this;
+  this.FooPlugin = this.game.plugins.get('voxel-health');
+  // Register listeners
+
+}
+
+FooPlugin.prototype.disable = function() {
+  // Remove Listeners
+}
+
+FooPlugin.foo(){
+  // This plugin can be called by the game
+  // Do something
+}
